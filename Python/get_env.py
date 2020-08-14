90f38698f4699ce6905cf307fb41bd572d560a80
Umlatt <theplacetosendmemail@gmail.com>
2020-08-14 11:33:40 +0200

    Added script comments.

--- Python/get_env.py
+++ Python/get_env.py
@@ -1,9 +1,30 @@
+########################################################################################################################
+#                                           Jelastic - Sample Script - GetEnv                                          #
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                                                                                                      #
+# INTRODUCTION:                                                                                                        #
+#   This script is a sample script to start you off with communicating to a Jelastic region.                           #
+#   The script requires that you have created an API token in the Jelastic UI, as you will need to provide it in       #
+#   the variables section.                                                                                             #
+#                                                                                                                      #
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                                                                                                      #
+# DETAILS:                                                                                                             #
+#   This script will connect, using the python REST API, and pull your environment details.                            #
+#                                                                                                                      #
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                                                                                                      #
+# CHANGELOG                                                                                                            #
+# v1.0 - 14 August 2020 (Richard Raymond)                                                                              #
+#   - Initial version                                                                                                  #
+#                                                                                                                      #
+########################################################################################################################
 import requests  # Required for RESTful comms
 import json  # Required for Jelastic restful responses
 
 # VARIABLES
 jelastic_region_url = 'https://app.demo.jelastic.com/1.0'
-jelastic_region_api_token = 'api_token'
+jelastic_region_api_token = '2ddce07e785d4e9fb3e395cce53f0dac248e16d3'
 jelastic_appid = '1dd8d191d38fff45e62564fcf67fdcd6'
 
 # REST - QUERIES
